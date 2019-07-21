cc0b60e0eb0e2d1aef30ce13f50c472e2723522b
rickaa <37962604+d0l-phin@users.noreply.github.com>
2019-07-21 21:18:49 +0200

    Update build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 FILES="$(find notebooks -type f -name '*.ipynb')"
 for f in $FILES
 do
-    nb2hugo $f --site-dir base --section post
+    nb2hugo $f --site-dir rand --section post
 done
-hugo -s base
+hugo -s rand
