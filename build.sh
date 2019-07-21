6fb728eca80bfffee992c706b655e213d679ccfc
rickaa <37962604+d0l-phin@users.noreply.github.com>
2019-07-22 01:15:02 +0200

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-FILES="$(find notebooks -type f -name '*.ipynb' ! -path '*_checkpoints*')"
+FILES="$(find notebooks -type f -name '*.ipynb' -not -path '*_checkpoints*')"
 for f in $FILES
 do
     nb2hugo $f --site-dir rand --section post
 done
-hugo -s rand
+cd rand
+hugo
