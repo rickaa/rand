8d0a061dbb909aafc6ac5fcd54f89712f107863a
rickaa <37962604+d0l-phin@users.noreply.github.com>
2020-05-12 19:20:57 +0200

    moving back

---
+++ rand/assets/css/postcss.config.js
@@ -0,0 +1,14 @@
+
+
+module.exports = {    
+  plugins: [
+    require('postcss-import')({
+      path: ["assets/css"],
+    }),         
+    require('tailwindcss')('./assets/css/tailwind.config.js'),       
+    require('autoprefixer')({
+      grid: true,
+      overrideBrowserslist: ['>1%']
+    }),    
+  ]
+}
