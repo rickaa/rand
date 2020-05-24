5991dfc25007c5c31de5db2d7589f9d52db4d945
rickaa <37962604+d0l-phin@users.noreply.github.com>
2020-05-24 22:02:35 +0200

    purgecss

--- rand/assets/css/postcss.config.js
+++ rand/assets/css/postcss.config.js
@@ -1,14 +1,22 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
 
-
-module.exports = {    
-  plugins: [
-    require('postcss-import')({
-      path: ["assets/css"],
-    }),         
-    require('tailwindcss')('./assets/css/tailwind.config.js'),       
-    require('autoprefixer')({
-      grid: true,
-      overrideBrowserslist: ['>1%']
-    }),    
-  ]
+module.exports = {
+    plugins: [
+        require('postcss-import')({
+            path: ["assets/css"],
+        }),
+        require('tailwindcss')('./assets/css/tailwind.config.js'),
+        require('autoprefixer')({
+            grid: true,
+            overrideBrowserslist: ['>1%']
+        }),
+        purgecss({
+            content: [
+                "./layouts/**/*.html"
+            ],
+            defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || []
+            // defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+        }),
+        require("postcss-preset-env")
+    ]
 }
