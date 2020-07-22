1c2bf3c87d642536768f5dc0dcebb0e8e8788e25
rickaa <37962604+d0l-phin@users.noreply.github.com>
2020-07-22 18:14:16 +0200

    comment out local build

--- local_build.sh
+++ local_build.sh
@@ -1,12 +1,12 @@
-FILES="$(find notebooks -type f -name '*.ipynb' -not -path '*_checkpoints*')"
-for f in $FILES
-do
-    nb2hugo $f --site-dir rand --section posts
-done
+# FILES="$(find notebooks -type f -name '*.ipynb' -not -path '*_checkpoints*')"
+# for f in $FILES
+# do
+#     nb2hugo $f --site-dir rand --section posts
+# done
 
-MKDS="$(find thoughts -type f -name '*.md' ! -name '_*')"
-for f in $MKDS
-do
-    echo "Creating post $f"
-    cp $f rand/content/thoughts
-done
+# MKDS="$(find thoughts -type f -name '*.md' ! -name '_*')"
+# for f in $MKDS
+# do
+#     echo "Creating post $f"
+#     cp $f rand/content/thoughts
+# done
