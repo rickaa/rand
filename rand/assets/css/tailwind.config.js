286fd24e8b33fb07cabb0d053d2a21a5aaff75d9
rickaa <37962604+d0l-phin@users.noreply.github.com>
2020-09-22 21:08:27 +0200

    u

--- rand/assets/css/tailwind.config.js
+++ rand/assets/css/tailwind.config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  purge: {
-    content: ['./layouts/**/*.html'],
-    options: {
-      whitelist: [],
-    }
-},
-  theme: {
-    extend: {
-      colors: {
-        gray: {
-          '100': '#fafafa',
-          '200': '#eeeeee',
-          '300': '#e0e0e0',
-          '400': '#bdbdbd',
-          '500': '#9e9e9e',
-          '600': '#757575',
-          '700': '#616161',
-          '800': '#424242',
-          '900': '#212121',
+    purge: {
+        content: ['../../layouts/**/*.html'],
+        options: {
+            whitelist: [],
         }
-      }
     },
-  },
-  variants: {},
-  plugins: [],
+    theme: {
+        extend: {
+            colors: {
+                gray: {
+                    '100': '#fafafa',
+                    '200': '#eeeeee',
+                    '300': '#e0e0e0',
+                    '400': '#bdbdbd',
+                    '500': '#9e9e9e',
+                    '600': '#757575',
+                    '700': '#616161',
+                    '800': '#424242',
+                    '900': '#212121',
+                }
+            }
+        },
+    },
+    variants: {},
+    plugins: [],
 };
